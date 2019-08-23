11855a56a65566bcc9a07f8fac1dd46f4c82ca6e
Huynh Vu <vdhuynh@tma.com.vn>
2019-08-23 09:47:34 +0700

    Build the registration form #1

--- frontend/src/modalwindows.js
+++ frontend/src/modalwindows.js
@@ -70,3 +70,65 @@ class SignInForm extends React.Component {
         );
     }
 }
+
+class Registeration extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            errormessage: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        let message = null;
+        if (this.state.errormessage.length !== 0) {
+            message = <h5 className="mb-4 text-danger">{this.state.errormessage}</h5>;
+        }
+        return (
+            <div>
+                {message}
+                <form onSubmit={this.handleSubmit}>
+                    <h5 className="mb-4">Registeration</h5>
+                    <div className="form-group">
+                        <label htmlFor="username">User Name:</label>
+                        <input id="username" name='username' className="form-control" placeholder='John Doe' type='text' onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name='email' className="form-control" id="email" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pass">Password:</label>
+                        <input type="password" name='pass1' className="form-control" id="pass1" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pass">Confirm password:</label>
+                        <input type="password" name='pass2' className="form-control" id="pass2" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-row text-center">
+                        <div className="col-12 mt-2">
+                            <button type="submit" className="btn btn-success btn-large">Register</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
